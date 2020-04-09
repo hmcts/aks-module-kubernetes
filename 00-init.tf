0856ef5d04fbccefcf9f9634faab9dccc8982019
stravze <stravze@gmail.com>
2020-04-09 15:01:11 +0100

    provider

--- 00-init.tf
+++ 00-init.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 0.12.0"
 }
+
+provider "azurerm" {
+  alias = "hmtcs-control"
+  skip_provider_registration = "true"
+  version         = ">= 1.44.0"
+  features {}
+  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+}
