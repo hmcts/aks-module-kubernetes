4d3fbf5d8e07c5ab4b5959f9e098fd262d70e4ad
Tim Jacomb <timjacomb1+github@gmail.com>
2020-10-09 11:12:46 +0100

    Remove providers

--- 00-init.tf
+++ 00-init.tf
@@ -2,10 +2,3 @@ terraform {
   required_version = ">= 0.13.0"
 }
 
-provider "azurerm" {
-  alias = "hmcts-control"
-  skip_provider_registration = "true"
-  version         = "= 2.31.0"
-  features {}
-  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
-}
