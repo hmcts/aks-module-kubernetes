777a22acc5a3260827352a5d037b2ccf87e699e2
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-13 14:21:38 +0100

    Cleanup

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -3,7 +3,7 @@ data "azurerm_subscription" "subscription" {}
 
 data "azurerm_role_definition" "builtin_role_definition" {
   name  = "Contributor"
-  scope = data.azurerm_subscription.subscription.id # /subscriptions/00000000-0000-0000-0000-000000000000
+  scope = data.azurerm_subscription.subscription.id
 }
 
 data "null_data_source" "tag_defaults" {
@@ -29,7 +29,7 @@ data "azurerm_log_analytics_workspace" "ss-law" {
     var.deploy_environment
   )
 
-  resource_group_name  = format("%s-%s-monitoring-rg",
+  resource_group_name = format("%s-%s-monitoring-rg",
     var.service_name_prefix,
     var.deploy_environment
   )
@@ -45,36 +45,6 @@ data "azurerm_subnet" "aks" {
   resource_group_name  = var.network_resource_group_name
 }
 
-# data "azurerm_subnet" "aks_01" {
-#   name = format("%s_01_%s",
-#     var.network_shortname,
-#     var.deploy_environment
-#   )
-
-#   virtual_network_name = var.network_name
-#   resource_group_name  = var.network_resource_group_name
-# }
-
-# data "azurerm_subnet" "iaas" {
-#   name = format("%s_iaas_%s",
-#     var.network_shortname,
-#     var.deploy_environment
-#   )
-
-#   virtual_network_name = var.network_name
-#   resource_group_name  = var.network_resource_group_name
-# }
-
-# data "azurerm_subnet" "application_gateway" {
-#   name = format("%s_application_gateway_%s",
-#     var.network_shortname,
-#     var.deploy_environment
-#   )
-
-#   virtual_network_name = var.network_name
-#   resource_group_name  = var.network_resource_group_name
-# }
-
 data "azurerm_key_vault" "hmcts_access_vault" {
   provider            = azurerm.hmcts-control
   name                = var.hmcts_access_vault
@@ -82,19 +52,19 @@ data "azurerm_key_vault" "hmcts_access_vault" {
 }
 
 data "azurerm_key_vault_secret" "kubernetes_aad_client_app_id" {
-  provider            = azurerm.hmcts-control
-  name                = "${var.service_shortname}-client-application-id"
-  key_vault_id        = data.azurerm_key_vault.hmcts_access_vault.id
+  provider     = azurerm.hmcts-control
+  name         = "${var.service_shortname}-client-application-id"
+  key_vault_id = data.azurerm_key_vault.hmcts_access_vault.id
 }
 
 data "azurerm_key_vault_secret" "kubernetes_aad_server_app_id" {
-  provider            = azurerm.hmcts-control
-  name                = "${var.service_shortname}-server-application-id"
-  key_vault_id        = data.azurerm_key_vault.hmcts_access_vault.id
+  provider     = azurerm.hmcts-control
+  name         = "${var.service_shortname}-server-application-id"
+  key_vault_id = data.azurerm_key_vault.hmcts_access_vault.id
 }
 
 data "azurerm_key_vault_secret" "kubernetes_aad_server_app_secret" {
-  provider            = azurerm.hmcts-control
-  name                = "${var.service_shortname}-server-token"
-  key_vault_id        = data.azurerm_key_vault.hmcts_access_vault.id
+  provider     = azurerm.hmcts-control
+  name         = "${var.service_shortname}-server-token"
+  key_vault_id = data.azurerm_key_vault.hmcts_access_vault.id
 }
