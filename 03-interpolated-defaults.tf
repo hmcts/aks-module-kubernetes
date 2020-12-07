f3da4052d12d9a0b3a83a2fb8568512694f6007e
mokainos <72137340+mokainos@users.noreply.github.com>
2020-12-07 09:45:42 +0000

    added variable

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -10,18 +10,6 @@ locals {
 }
 
 
-data "azurerm_log_analytics_workspace" "ss-law" {	
-  name = format("%s-%s-law",	
-    var.project,	
-    var.environment	
-  )	
-
-  resource_group_name = format("%s-%s-monitoring-rg",	
-    var.project,	
-    var.environment	
-  )	
-}
-
 
 
 data "azurerm_subnet" "aks" {
