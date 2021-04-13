7a642cbeb7140eb1c2c27e862b20c6e96e1501eb
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-04-13 16:38:46 +0100

    remove types

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -70,15 +70,7 @@ variable "kubernetes_cluster_admin_username" {
 variable "kubernetes_cluster_ssh_key" {}
 
 variable "additional_node_pools" {
-  type = list(object({
-    name                = string
-    vm_size             = string
-    min_count           = number
-    max_count           = number
-    os_type             = string
-    node_taints         = list(string)
-    enable_auto_scaling = bool
+  type = list(object())
 
-  }))
   default = []
 }
