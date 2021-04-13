676fb89e8d505281b3b59d0279a4d4b3664b238b
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-04-13 16:48:39 +0100

    changing it back

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -70,7 +70,7 @@ variable "kubernetes_cluster_admin_username" {
 variable "kubernetes_cluster_ssh_key" {}
 
 variable "additional_node_pools" {
-  type = list()
+  type = list(object())
 
   default = []
 }
