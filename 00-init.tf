2927f515e5b139b11d7e2793fa130a2c10616398
msl8r <72858053+msl8r@users.noreply.github.com>
2021-06-04 10:55:41 +0100

    DTSPO-1032 - clean up provider configuration to resolve warnings

--- 00-init.tf
+++ 00-init.tf
@@ -1,15 +1,26 @@
 terraform {
   required_version = ">= 0.13.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.57.0"
+      configuration_aliases = [ azurerm.hmcts-control, azurerm.acr, azurerm.global_acr  ]
+    }
+  }
 }
 
-provider "azurerm" {
-  alias = "hmcts-control"
-}
+# terraform {
+  
+# }
 
-provider "azurerm" {
-  alias = "acr"
-}
+# provider "azurerm" {
+#   alias = "hmcts-control"
+# }
 
-provider "azurerm" {
-  alias = "global_acr"
-}
+# provider "azurerm" {
+#   alias = "acr"
+# }
+
+# provider "azurerm" {
+#   alias = "global_acr"
+# }
