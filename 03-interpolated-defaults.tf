2fb6a5298672ea1acd0e1b30505dc5e937041199
Avi <avinash.mutham@hmcts.net>
2021-06-09 13:18:33 +0100

    clean up

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -8,7 +8,7 @@ data "azurerm_role_definition" "builtin_role_definition" {
 locals {
   slug_location = lower(replace(var.location, " ", "."))
 
-  environment = (var.environment == "sbox") ? "sandbox" : ( (var.environment == "test") ? "perftest" : "${var.environment}")
+  environment = (var.environment == "sbox") ? "sandbox" : (var.environment == "test") ? "perftest" : "${var.environment}"
   
 }
 
