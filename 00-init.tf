5326094d5f93a62e9983dc58744cc45ae1f514bd
Thomas Thornton <>
2021-06-17 13:49:28 +0100

    Push branch cft_refactor

--- 00-init.tf
+++ 00-init.tf
@@ -1,19 +1,10 @@
 terraform {
   required_version = ">= 0.13.0"
-}
-
-provider "azurerm" {
-  alias = "hmcts-control"
-}
-
-provider "azurerm" {
-  alias = "acr"
-}
-
-provider "azurerm" {
-  alias = "global_acr"
-}
-
-provider "azurerm" {
-  alias = "mi_cft"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.57.0"
+      configuration_aliases = [ azurerm.hmcts-control, azurerm.acr, azurerm.global_acr, azurerm.mi_cft  ]
+    }
+  }
 }
