f2173e73359f746bcc0bf91b1977322a5524379a
Thomas Thornton <>
2021-06-24 16:52:15 +0100

    no message

--- 00-init.tf
+++ 00-init.tf
@@ -1,10 +1,15 @@
 terraform {
   required_version = ">= 0.13.0"
-  required_providers {
-    azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = "2.57.0"
-      configuration_aliases = [azurerm.hmcts-control, azurerm.acr, azurerm.global_acr]
-    }
-  }
+}
+
+provider "azurerm" {
+  alias = "hmcts-control"
+}
+
+provider "azurerm" {
+  alias = "acr"
+}
+
+provider "azurerm" {
+  alias = "global_acr"
 }
