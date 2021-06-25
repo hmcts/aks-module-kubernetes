82a6466a8fc6103fc7436bfdcc56e483177d36b4
Thomas Thornton <>
2021-06-25 08:23:26 +0100

    swap provider

--- 00-init.tf
+++ 00-init.tf
@@ -1,30 +1,30 @@
-# terraform {
-#   required_version = ">= 0.13.0"
-# }
-
-# provider "azurerm" {
-#   alias = "hmcts-control"
-# }
-
-# provider "azurerm" {
-#   alias = "acr"
-# }
-
-# provider "azurerm" {
-#   alias = "global_acr"
-# }
-
-# provider "azurerm" {
-#   alias = "mi_cft"
-# }
-
 terraform {
   required_version = ">= 0.13.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.57.0"
-      configuration_aliases = [ azurerm.hmcts-control, azurerm.acr, azurerm.global_acr  ]
-    }
-  }
 }
+
+provider "azurerm" {
+  alias = "hmcts-control"
+}
+
+provider "azurerm" {
+  alias = "acr"
+}
+
+provider "azurerm" {
+  alias = "global_acr"
+}
+
+provider "azurerm" {
+  alias = "mi_cft"
+}
+
+# terraform {
+#   required_version = ">= 0.13.0"
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "2.57.0"
+#       configuration_aliases = [ azurerm.hmcts-control, azurerm.acr, azurerm.global_acr  ]
+#     }
+#   }
+# }
