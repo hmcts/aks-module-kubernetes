98c0a00383fb3a8b5882a3584940a7b4ecd0bbb3
mokainos <72137340+mokainos@users.noreply.github.com>
2021-06-28 11:31:35 +0100

    changed to principal id

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -144,7 +144,7 @@ data "azurerm_resource_group" "disks_resource_group" {
 }
 
 resource "azurerm_role_assignment" "disks_resource_group_role_assignment" {
-  principal_id         = data.azurerm_user_assigned_identity.aks.id
+  principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
   scope                = data.azurerm_resource_group.disks_resource_group.id
   role_definition_name = "Virtual Machine Contributor"
 }
