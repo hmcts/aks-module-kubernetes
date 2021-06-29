792339ebf509fb1aa61ab963a24143933bd4c068
Thomas Thornton <>
2021-06-29 08:33:02 +0100

    Update to original provider setup

--- 00-init.tf
+++ 00-init.tf
@@ -1,23 +1,3 @@
-# terraform {
-#   required_version = ">= 0.13.0"
-# }
-
-# provider "azurerm" {
-#   alias = "hmcts-control"
-# }
-
-# provider "azurerm" {
-#   alias = "acr"
-# }
-
-# provider "azurerm" {
-#   alias = "global_acr"
-# }
-
-# provider "azurerm" {
-#   alias = "mi_cft"
-# }
-
 terraform {
   required_version = ">= 0.13.0"
   required_providers {
