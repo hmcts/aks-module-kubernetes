e6a6f1d5b1459da80355c94efdfe5f6c8d8a9467
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-10 11:16:08 +0100

    DTSPO-4676 - fix typo

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -35,8 +35,8 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
 
   sku_tier = var.sku_tier
   default_node_pool {
-    name                 = var.enable_user_system_nodepool_split == "true" ? "system" : "nodepool"
-    node_taints          = var.enable_user_system_nodepool_split == "true" ? ["CriticalAddonsOnly=true:NoSchedule"] : []
+    name                 = var.enable_user_system_nodepool_split == true ? "system" : "nodepool"
+    node_taints          = var.enable_user_system_nodepool_split == true ? ["CriticalAddonsOnly=true:NoSchedule"] : []
     vm_size              = var.kubernetes_cluster_agent_vm_size
     enable_auto_scaling  = var.kubernetes_cluster_enable_auto_scaling
     max_pods             = var.kubernetes_cluster_agent_max_pods
