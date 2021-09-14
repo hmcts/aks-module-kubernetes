d895d058e404778e9dbcfef0714c70b4f933f184
msl8r <72858053+msl8r@users.noreply.github.com>
2021-09-14 16:54:13 +0100

    Downgrade AzureRM to 2.75

--- 00-init.tf
+++ 00-init.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "2.76.0"
+      version               = "2.75.0"
       configuration_aliases = [azurerm.hmcts-control, azurerm.acr, azurerm.global_acr]
     }
   }
