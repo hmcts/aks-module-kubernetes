5f3291f01dc8598672251181dc82a31d583de447
Enda Kelly <enda.kelly@hmcts.net>
2022-02-03 17:12:46 +0000

    Add public ip

---
+++ public-ip.tf
@@ -0,0 +1,9 @@
+resource "azurerm_public_ip" "example" {
+  count = var.environment == "demo" ? 1 : 0
+  name                = "${var.project}-${var.environment}-public-ip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+
+  tags = var.tags
+}
