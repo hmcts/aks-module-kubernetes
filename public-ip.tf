7f315791a4535ccee34777710705128a3f71a3c1
Enda Kelly <enda.kelly@hmcts.net>
2022-02-04 09:33:52 +0000

    update pip name

--- public-ip.tf
+++ public-ip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "example" {
   count = var.environment == "demo" ? 1 : 0
-  name                = "${var.project}-${var.environment}-public-ip-${var.cluster_number}"
+  name                = "${var.project}-${var.environment}-${var.cluster_number}-public-ip"
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = "Static"
