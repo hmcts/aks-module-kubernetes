e220f05d1562450d3597fbcb0bd82516d2421370
Zoe Cope <zoe.cope@hmcts.net>
2022-04-05 16:41:02 +0100

    remove output

--- outputs.tf
+++ outputs.tf
@@ -5,9 +5,3 @@ output "cluster" {
 output "kubelet_object_id" {
   value = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
 }
-
-output "kubelet_identity" {
-  value = {
-    user_assigned_identity_id = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].user_assigned_identity_id
-  }
-}
