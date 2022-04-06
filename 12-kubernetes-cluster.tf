dccb94e24fc0a97cdd138d399247e44ee7d36eca
Zoe Cope <zoe.cope@hmcts.net>
2022-04-06 10:56:38 +0100

    formatting

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -65,7 +65,7 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   kubelet_identity {
     client_id                 = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].client_id
     object_id                 = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
-    user_assigned_identity_id = zurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].user_assigned_identity_id
+    user_assigned_identity_id = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].user_assigned_identity_id
   }
 
   oms_agent {
