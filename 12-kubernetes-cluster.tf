d9c6b4b902817f4ac943a935f94f2675b7ff11ae
Zoe Cope <zoe.cope@hmcts.net>
2022-04-06 11:04:18 +0100

    add count back

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -63,9 +63,8 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   }
 
   kubelet_identity {
-    client_id                 = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].client_id
-    object_id                 = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
-    user_assigned_identity_id = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].user_assigned_identity_id
+    count                     = var.kubelet_uami_enabled == true ? 1 : 0
+    user_assigned_identity_id = [data.azurerm_user_assigned_identity.aks.id]
   }
 
   oms_agent {
