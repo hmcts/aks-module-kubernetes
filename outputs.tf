15317e6168be3450f195aaaccfb5711f508b216e
Zoe Cope <zoe.cope@hmcts.net>
2022-04-07 10:03:52 +0100

    remove outputs

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "cluster" {
 output "kubelet_object_id" {
   value = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
 }
-
-output "kubelet_uami_enabled" {
-  value = var.kubelet_uami_enabled
-}
