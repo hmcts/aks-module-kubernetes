7d2c1c2371ca811af666b8edc55961643cd67e09
Zoe Cope <zoe.cope@hmcts.net>
2022-04-19 15:58:53 +0100

    output node rg name

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,10 @@ output "kubelet_object_id" {
   value = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
 }
 
+output "kubelet_object_id" {
+  value = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+}
+
+output "node_resource_group" {
+  value = azurerm_kubernetes_cluster.kubernetes_cluster.node_resource_group.name
+}
