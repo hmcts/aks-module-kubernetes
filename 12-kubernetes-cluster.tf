3240bcaecf35e54c7bfec186a61798f3a0ab6f5c
Zoë <54357991+zc-hmcts@users.noreply.github.com>
2022-04-20 11:23:18 +0100

    Update 12-kubernetes-cluster.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -168,7 +168,7 @@ data "azurerm_resource_group" "disks_resource_group" {
 
 resource "azurerm_role_assignment" "disks_resource_group_role_assignment" {
   count                = var.ptl_cluster ? 1 : 0
-  principal_id         = data.azurerm_user_assigned_identity.aks[0].principal_id
+  principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
   scope                = data.azurerm_resource_group.disks_resource_group.id
   role_definition_name = "Virtual Machine Contributor"
 }
