e5bd5c41c43df4f3d81cfb256f91b6512a6b5d95
Zoe Cope <zoe.cope@hmcts.net>
2022-04-25 13:10:28 +0100

    flag

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -117,16 +117,22 @@ resource "azurerm_role_assignment" "genesis_managed_identity_operator" {
   principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
   scope                = data.azurerm_user_assigned_identity.aks.id
   role_definition_name = "Managed Identity Operator"
+
+  count = var.kubelet_uami_enabled ? 0 : 1
 }
 
 resource "azurerm_role_assignment" "uami_rg_identity_operator" {
   principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
   scope                = data.azurerm_resource_group.managed-identity-operator.id
   role_definition_name = "Managed Identity Operator"
+
+  count = var.kubelet_uami_enabled ? 0 : 1
 }
 
 data "azurerm_resource_group" "node_resource_group" {
   name = azurerm_kubernetes_cluster.kubernetes_cluster.node_resource_group
+
+  count = var.kubelet_uami_enabled ? 0 : 1
 }
 
 resource "azurerm_role_assignment" "node_infrastructure_update_scale_set" {
