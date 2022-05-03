98b4c0bd04b2b68322f6f42d8875840aa2e91f7e
msl8r <72858053+msl8r@users.noreply.github.com>
2022-05-03 09:23:37 +0100

    remove id

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -131,7 +131,7 @@ resource "azurerm_role_assignment" "uami_rg_identity_operator" {
 
 resource "azurerm_role_assignment" "node_infrastructure_update_scale_set" {
   principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
-  scope                = azurerm_kubernetes_cluster.kubernetes_cluster.node_resource_group.id
+  scope                = azurerm_kubernetes_cluster.kubernetes_cluster.node_resource_group
   role_definition_name = "Virtual Machine Contributor"
 }
 
