e16a7d96c25619307c0011a25ad3757bc6563eeb
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-14 12:53:28 +0100

    DTSPO-8276 - upgrade terraform and azure provider

--- 00-init.tf
+++ 00-init.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.2.2"
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "3.0.2"
+      version               = "3.10.0"
       configuration_aliases = [azurerm.hmcts-control, azurerm.acr, azurerm.global_acr]
     }
   }
