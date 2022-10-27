171b41b311a73df66adc1bf910321534f97fed17
Ciarán Carragher <c.carragher@kainos.com>
2022-10-27 22:20:46 +0100

    Adds cluster number to federated indetity credential name

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -254,7 +254,7 @@ resource "azurerm_role_assignment" "Contributor" {
 
 resource "azapi_resource" "federated_identity_credential" {
   schema_validation_enabled = false
-  name                      = "aso-federated-credential"
+  name                      = "aso-federated-credential"-"${var.cluster_number}"
   parent_id                 = data.azurerm_user_assigned_identity.aks.id
   type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
   location                  = var.location
