1607feb6d61757aabc34c9f8cb7a35574e5c7b73
ciaranca <c.carragher@kainos.com>
2022-11-01 10:10:11 +0000

    Update 00-init.tf

--- 00-init.tf
+++ 00-init.tf
@@ -5,7 +5,7 @@ terraform {
       source                = "hashicorp/azurerm"
       configuration_aliases = [azurerm.hmcts-control, azurerm.acr, azurerm.global_acr]
     }
-     azapi = {
+    azapi = {
       source  = "Azure/azapi"
       version = "1.0.0"
     }
