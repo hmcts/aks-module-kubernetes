0d028a16582cc2a4ddeb2c15ccc01763cc2fc48e
Louise Huyton <louise.huyton@justice.gov.uk>
2022-12-13 11:39:19 +0000

    Update aks-auto-shutdown-permissions.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- aks-auto-shutdown-permissions.tf
+++ aks-auto-shutdown-permissions.tf
@@ -1,7 +1,7 @@
 resource "azurerm_role_assignment" "aks_auto_shutdown" {
   count = var.aks_auto_shutdown_principal_id == "" ? 0 : 1
 
-  principal_id         = data.azurerm_user_assigned_managed_identity.aks_auto_shutdown.principal_id
+  principal_id         = var.aks_auto_shutdown_principal_id
   scope                = azurerm_kubernetes_cluster.kubernetes_cluster.id
   role_definition_name = "Contributor"
 }
