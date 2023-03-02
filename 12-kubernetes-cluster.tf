dcb2545a5a6ffad5fb1f4e899acab2f6067112ef
Rees Pozzi <reespozzi@icloud.com>
2023-03-02 10:08:53 +0000

    Switch to adding description

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -245,12 +245,9 @@ resource "azurerm_monitor_diagnostic_setting" "kubernetes_cluster_diagnostic_set
 }
 
 # Azure Service Operator federated identity credential and role assignment
-resource "random_uuid" "aso_role_assignment_name" {
-  keepers = var.cluster_number
-}
 resource "azurerm_role_assignment" "service_operator" {
   count                = var.service_operator_settings_enabled ? 1 : 0
-  name                 = var.random_uuid.aso_role_assignment_name
+  description          = "Service operator role asssignment for ${var.environment}-${var.cluster_number}"
   principal_id         = data.azurerm_user_assigned_identity.aks.principal_id
   role_definition_name = "Contributor"
   scope                = data.azurerm_subscription.subscription.id
