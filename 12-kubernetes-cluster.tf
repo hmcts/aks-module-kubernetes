d44d3f0b104aee9c2386f04c4c3781f592e31e34
Louise Huyton <louise.huyton@justice.gov.uk>
2023-10-31 10:22:19 +0000

    try this

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -180,8 +180,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "additional_node_pools" {
 
 variable "flux_project" {
   type        = string
-  default     = contains("ss", var.project) ? "sds" :  "cft"
+  default     = "cft"
 }
+
+flux_project = contains("ss", var.project) ? "sds" :  "cft"
 
 resource "azurerm_kubernetes_cluster_extension" "microsoft_flux_extension" {
   name           = "microsoft-flux-extension"
