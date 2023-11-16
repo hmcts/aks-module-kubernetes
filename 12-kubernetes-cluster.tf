85999ff9ad72eb6efb992c8ebe13a48fe0a8bef9
Louise Huyton <louise.huyton@justice.gov.uk>
2023-11-16 15:00:25 +0000

    Remove git-credentials local auth

--- 12-kubernetes-cluster.tf
+++ 12-kubernetes-cluster.tf
@@ -193,8 +193,7 @@ resource "azurerm_kubernetes_flux_configuration" "microsoft_flux_configuration" 
   git_repository {
     url             = "https://github.com/hmcts/${contains(["ss"], var.project) ? "sds" :  "cnp"}-flux-config"
     reference_type  = "branch"
-    reference_value = "DTSPO-8700"
-    local_auth_reference = "git-credentials"
+    reference_value = "DTSPO-8700
   }
 
   kustomizations {
