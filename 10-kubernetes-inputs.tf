2cd121695bb4845bace76ab8003f88ef035476f1
Joshua Glass <141819030+JoshuaGlassKainos@users.noreply.github.com>
2024-03-21 16:57:22 +0000

    Update 10-kubernetes-inputs.tf
    Co-authored-by: Rees Pozzi <rees.pozzi@justice.gov.uk>

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -137,7 +137,7 @@ variable "node_os_maintenance_window_config" {
     condition = (
       var.node_os_maintenance_window_config.frequency == "Daily" && var.node_os_maintenance_window_config.day_of_week == null) || (
       var.node_os_maintenance_window_config.frequency == "Weekly" &&
-    try(contains(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], var.node_os_maintenance_window_config.day_of_week), false))
+      try(contains(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], var.node_os_maintenance_window_config.day_of_week), false))
     error_message = var.node_os_maintenance_window_config.frequency == "Daily" ? "'day_of_week' must not be set for 'Daily' frequency." : "Invalid 'day_of_week', please choose a day of the week."
   }
 }
