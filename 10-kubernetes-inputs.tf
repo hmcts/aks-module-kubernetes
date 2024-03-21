8f44b852ac18e864ed9a95bce6effeb381b30690
Joshua Glass <joshua.glass@justice.gov.uk>
2024-03-21 10:35:08 +0000

    revert error msg

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -132,11 +132,11 @@ variable "node_os_maintenance_window_config" {
   }
   validation {
     condition     = var.node_os_maintenance_window_config.frequency == "Daily" && ((try(contains(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], var.node_os_maintenance_window_config.day_of_week), false)))
-    error_message = "Invalid day_of_week must not be set for 'Daily' frequency."
+    error_message = "Invalid day_of_week. Please choose from Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday."
   }
 
   validation {
     condition     = var.node_os_maintenance_window_config.frequency == "Weekly" && (var.node_os_maintenance_window_config.day_of_week == null)
-    error_message = "Invalid day_of_week. Please choose from Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday."
+    error_message = "Invalid day_of_week must not be set for 'Daily' frequency."
   }
 }
