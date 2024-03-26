8c1fc53d973a9b7b8e2170e9d96d674057def2b4
Joshua Glass <joshua.glass@justice.gov.uk>
2024-03-26 15:29:13 +0000

    reduce range for prod maintenance window

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -112,8 +112,8 @@ variable "node_os_maintenance_window_config" {
 
   validation {
     condition = var.node_os_maintenance_window_config.is_prod ? (tonumber(substr(var.node_os_maintenance_window_config.start_time, 0, 2)) >= 23
-    ) || (tonumber(substr(var.node_os_maintenance_window_config.start_time, 0, 2)) <= 6) : true
-    error_message = "Invalid 'start_time' Prod Use must only start between 23:00 - 06:00"
+    ) || (tonumber(substr(var.node_os_maintenance_window_config.start_time, 0, 2)) <= 2) : true
+    error_message = "Invalid 'start_time' Prod Use must only start between 23:00 - 02:00"
   }
 
   validation {
