0172786aff7e1fba771afc84a4ad5a71dc9a2179
Joshua Glass <joshua.glass@justice.gov.uk>
2024-03-26 12:12:31 +0000

    updated is_prod condition

--- 10-kubernetes-inputs.tf
+++ 10-kubernetes-inputs.tf
@@ -111,8 +111,9 @@ variable "node_os_maintenance_window_config" {
   default = {}
 
   validation {
-    condition = var.node_os_maintenance_window_config.is_prod != true
-    error_message = "is_prod is set to true"
+    condition = var.node_os_maintenance_window_config.is_prod == true && (tonumber(substr(var.node_os_maintenance_window_config.start_time, 0, 2)) >= 23
+    ) || (tonumber(substr(var.node_os_maintenance_window_config.start_time, 0, 2)) <= 6)
+    error_message = "Invalid 'start_time' must only start between 23:00 - 06:00"
   }
 
   validation {
