14c2540bd387a500286bb78b90d32167ae3e4769
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-10-23 13:32:01 +0100

    Update service-operator.tf

--- service-operator.tf
+++ service-operator.tf
@@ -4,13 +4,13 @@ resource "azapi_resource" "service_operator_credential" {
   parent_id                 = data.azurerm_user_assigned_identity.aks.id
   type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
   location                  = var.location
-  body = {
+  body = jsonencode({
     properties = {
       issuer    = azurerm_kubernetes_cluster.kubernetes_cluster.oidc_issuer_url
       subject   = "system:serviceaccount:azureserviceoperator-system:azureserviceoperator-default"
       audiences = ["api://AzureADTokenExchange"]
     }
-  }
+  })
   lifecycle {
     ignore_changes = [location]
   }
