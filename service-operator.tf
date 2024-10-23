9df8c3197a233869bbc14ff7d6deaa8f56646062
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-10-23 13:53:23 +0100

    Update service-operator.tf

--- service-operator.tf
+++ service-operator.tf
@@ -1,17 +1,17 @@
-# resource "azapi_resource" "service_operator_credential" {
-#   schema_validation_enabled = false
-#   name                      = "${var.project}-${var.environment}-${var.cluster_number}-${var.service_shortname}"
-#   parent_id                 = data.azurerm_user_assigned_identity.aks.id
-#   type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
-#   location                  = var.location
-#   body = jsonencode({
-#     properties = {
-#       issuer    = azurerm_kubernetes_cluster.kubernetes_cluster.oidc_issuer_url
-#       subject   = "system:serviceaccount:azureserviceoperator-system:azureserviceoperator-default"
-#       audiences = ["api://AzureADTokenExchange"]
-#     }
-#   })
-#   lifecycle {
-#     ignore_changes = [location]
-#   }
-# }
+resource "azapi_resource" "service_operator_credential" {
+  schema_validation_enabled = false
+  name                      = "${var.project}-${var.environment}-${var.cluster_number}-${var.service_shortname}"
+  parent_id                 = data.azurerm_user_assigned_identity.aks.id
+  type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
+  location                  = var.location
+  body = jsonencode({
+    properties = {
+      issuer    = azurerm_kubernetes_cluster.kubernetes_cluster.oidc_issuer_url
+      subject   = "system:serviceaccount:azureserviceoperator-system:azureserviceoperator-default"
+      audiences = ["api://AzureADTokenExchange"]
+    }
+  })
+  lifecycle {
+    ignore_changes = [location]
+  }
+}
