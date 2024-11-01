82aa8473477a87a5bba38e0d50a0c9af159f6483
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-11-01 12:47:30 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "cluster" {
 output "kubelet_object_id" {
   value = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
 }
+
+output "aks_user_assigned_identity_id" {
+  value = data.azurerm_user_assigned_identity.aks.id
+}
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.kubernetes_cluster.oidc_issuer_url
+}
